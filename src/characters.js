1ca6b98649a666949a8207f7b910304e604d6e84
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-04-27 14:23:24 -0400

    chore: Add character images

---
+++ src/characters.js
@@ -0,0 +1,23 @@
+import MabelImg from ".assets/characters/Mabel_Pines.png";
+import DipperImg from ".assets/characters/Dipper_Pines.png";
+import StanImg from "./assets/characters/Stan_Pines.png";
+import WaddlesImg from "./Waddles.jpg";
+import SoosImg from "./assets/characters/Soos.jpg";
+import FordImg from "./assets/characters/Ford/jpg";
+import GrendaImg from "./assets/characters/Grenda.jpg";
+import WendyImg from "./assets/characters/Wendy.png";
+import TobyImg from "./assets/characters/Toby.jpg";
+import CandyImg from ".assets/characters/Candy.jpg";
+
+export const characters = [
+  { name: "Mabel", src: MabelImg },
+  { name: "Dipper", src: DipperImg },
+  { name: "Stan", src: StanImg },
+  { name: "Waddles", src: WaddlesImg },
+  { name: "Soos", src: SoosImg },
+  { name: "Ford", src: FordImg },
+  { name: "Grenda", src: GrendaImg },
+  { name: "Wendy", src: WendyImg },
+  { name: "Toby", src: TobyImg },
+  { name: "Candy", src: CandyImg },
+];
