f968cc89720c4053c0bd6007b88e79d3e2b8b6a1
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-04-27 15:35:58 -0400

    fix: Fixed incorrect image urls

--- src/characters.js
+++ src/characters.js
@@ -1,13 +1,13 @@
-import MabelImg from ".assets/characters/Mabel_Pines.png";
-import DipperImg from ".assets/characters/Dipper_Pines.png";
+import MabelImg from "./assets/characters/Mabel_Pines.png";
+import DipperImg from "./assets/characters/Dipper_Pines.png";
 import StanImg from "./assets/characters/Stan_Pines.png";
-import WaddlesImg from "./Waddles.jpg";
+import WaddlesImg from "./assets/characters/Waddles.jpg";
 import SoosImg from "./assets/characters/Soos.jpg";
-import FordImg from "./assets/characters/Ford/jpg";
+import FordImg from "./assets/characters/Ford.jpg";
 import GrendaImg from "./assets/characters/Grenda.jpg";
 import WendyImg from "./assets/characters/Wendy.png";
 import TobyImg from "./assets/characters/Toby.jpg";
-import CandyImg from ".assets/characters/Candy.jpg";
+import CandyImg from "./assets/characters/Candy.jpg";
 import { v4 as uuidv4 } from "uuid";
 
 export const characters = [
