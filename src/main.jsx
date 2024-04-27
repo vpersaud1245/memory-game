2e38989cf6a9e7e8c3b2b872f5e788ca66fb9085
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-04-27 15:38:07 -0400

    fix: Change rendered component

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/normalize.css";
 import "./index.css";
-import DifficultyMenu from "./components/DifficultyMenu";
+import MemoryCard from "./components/MemoryCard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <DifficultyMenu />
+    <MemoryCard />
   </React.StrictMode>
 );
