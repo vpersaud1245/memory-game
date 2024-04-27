73590ca7b934419b24534250dfe8b401b9970c25
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-04-27 14:30:40 -0400

    feat: Add uuid to character objects

--- src/characters.js
+++ src/characters.js
@@ -8,16 +8,17 @@ import GrendaImg from "./assets/characters/Grenda.jpg";
 import WendyImg from "./assets/characters/Wendy.png";
 import TobyImg from "./assets/characters/Toby.jpg";
 import CandyImg from ".assets/characters/Candy.jpg";
+import { v4 as uuidv4 } from "uuid";
 
 export const characters = [
-  { name: "Mabel", src: MabelImg },
-  { name: "Dipper", src: DipperImg },
-  { name: "Stan", src: StanImg },
-  { name: "Waddles", src: WaddlesImg },
-  { name: "Soos", src: SoosImg },
-  { name: "Ford", src: FordImg },
-  { name: "Grenda", src: GrendaImg },
-  { name: "Wendy", src: WendyImg },
-  { name: "Toby", src: TobyImg },
-  { name: "Candy", src: CandyImg },
+  { id: uuidv4(), name: "Mabel", src: MabelImg },
+  { id: uuidv4(), name: "Dipper", src: DipperImg },
+  { id: uuidv4(), name: "Stan", src: StanImg },
+  { id: uuidv4(), name: "Waddles", src: WaddlesImg },
+  { id: uuidv4(), name: "Soos", src: SoosImg },
+  { id: uuidv4(), name: "Ford", src: FordImg },
+  { id: uuidv4(), name: "Grenda", src: GrendaImg },
+  { id: uuidv4(), name: "Wendy", src: WendyImg },
+  { id: uuidv4(), name: "Toby", src: TobyImg },
+  { id: uuidv4(), name: "Candy", src: CandyImg },
 ];
