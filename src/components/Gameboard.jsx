f81c4b0dd93c46fe77a607ab5bcba0a7e82066c7
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-04-27 15:50:59 -0400

    feat: Add functions populate card components with random characters

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { characters } from "../characters";
 
 function getNumOfCardsFromDifficulty(difficulty) {
   if (difficulty === "easy") return 3;
@@ -6,18 +7,43 @@ function getNumOfCardsFromDifficulty(difficulty) {
   if (difficulty === "hard") return 5;
 }
 
+function getCardData(numOfCards) {
+  // Randomize the array order
+  const shuffledCharacterData = characters.toSorted(function () {
+    return 0.5 - Math.random();
+  });
+
+  // Select data based on number of cards needed
+  return shuffledCharacterData.slice(0, numOfCards);
+}
+
+function Card({ characterName, characterSrc }) {
+  /* REMOVE INLINE CSS STYLES */
+  return (
+    <div
+      className="card"
+      style={{ height: "100px", width: "100px", border: "1px solid gray" }}
+    >
+      <img
+        src={characterSrc}
+        alt={characterName + "Img"}
+        style={{ height: "100px", width: "100px" }}
+      />
+      <div className="characterName">{characterName}</div>
+    </div>
+  );
+}
+
 export default function Gameboard({ difficulty }) {
   const numOfCards = getNumOfCardsFromDifficulty(difficulty);
+  const cardData = getCardData(numOfCards);
 
-  const cards = [];
-  for (let i = 0; i < numOfCards; i += 1) {
-    cards.push(
-      <div
-        className="card"
-        style={{ height: "100px", width: "100px", border: "1px solid gray" }} // REMOVE ME
-        key={i}
-      ></div>
-    );
-  }
+  const cards = cardData.map((cardInfo) => (
+    <Card
+      key={cardInfo.id}
+      characterName={cardInfo.characterName}
+      characterSrc={cardInfo.src}
+    />
+  ));
   return <div className="gameboard">{cards}</div>;
 }
