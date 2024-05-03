6dcec2ac76e9c2642e58f085b938426821246da9
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-05-03 15:47:25 -0400

    style: Add background video styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./styles/normalize.css";
 import "./index.css";
 import MemoryCard from "./components/MemoryCard";
+import BackgroundVideo from "./components/BackgroundVideo";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MemoryCard />
+    <BackgroundVideo />
+    <div className="memory-card__app">
+      <MemoryCard />
+    </div>
   </React.StrictMode>
 );
