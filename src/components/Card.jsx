6d6c2663280ee0f44c5467fc35ffff9bccfab117
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-05-04 16:12:09 -0400

    feat: Add card styles and tilt animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,31 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useRef } from "react";
 import "../styles/card.css";
-import cardCover from "../assets/images/card-background.jpg";
+import VanillaTilt from "vanilla-tilt";
 export default function Card({ cardInfo, onClick }) {
+  const cardRef = useRef(null);
+
+  useEffect(() => {
+    const cardElement = cardRef.current;
+    VanillaTilt.init(cardElement, {
+      max: 25,
+      speed: 400,
+      glare: true,
+      "max-glare": 0.5,
+    });
+
+    return () => {
+      cardElement.vanillaTilt.destroy();
+    };
+  }, []);
+
   return (
-    <button
-      className="card"
-      onClick={onClick}
-      style={{
-        backgroundImage: `url(${cardInfo.src})`,
-      }}
-    >
-      <img src={cardCover} alt="" className="card__cover" />
-      <div className="characterName">{cardInfo.name}</div>
+    <button ref={cardRef} className="card" onClick={onClick}>
+      <div
+        className="card__img"
+        style={{ backgroundImage: `url(${cardInfo.src})` }}
+      ></div>
+      <div className="character__name">{cardInfo.name}</div>
     </button>
   );
 }
-
-/**FIGURING OUT HOW TO STYLE CARDS */
